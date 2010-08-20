a210aa06499e524c399b3c7e3f71c9a195fb54d3
waratuman <waratuman@gmail.com>
2010-08-20 11:36:58 -0700

    Server running.

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var fs = require('fs'),
 	url = require('url'),
 	http = require('http'),
 	path = require('path'),
+	mime = require('mime'),
 	redis = require('redis-client');
 	
 var db = redis.createClient(9281, 'goosefish.redistogo.com');
 var dbAuth = function() { db.auth('dc64f7b818f4e3ec2e3d3d033e3e5ff4'); }
 db.addListener('connected', dbAuth);
 db.addListener('reconnected', dbAuth);
+dbAuth();
 
 var httpServer = http.createServer( function(request, response) {
 	var pathname = url.parse(request.url).pathname;
@@ -26,7 +28,7 @@ var httpServer = http.createServer( function(request, response) {
 			return;
 		}
 		
-		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.writeHead(200, {'Content-Type': mime.lookup(filename)});
 		fs.createReadStream(filename, {
 			'flags': 'r',
 			'encoding': 'binary',
@@ -40,15 +42,15 @@ var httpServer = http.createServer( function(request, response) {
 	});
 });
 
-db.subscribeTo("flight_stream", function(channel, message, pattern) {
+var server = ws.createServer({}, httpServer);
+
+db.subscribeTo("*", function(channel, message, pattern) {
 	try { var flight = JSON.parse(message); }
 	catch (SyntaxError) { return false; }
-	
+
 	if ( flight.origin.iata == "BOS" || flight.destination.iata == "BOS") {
-		server.broadcast(flight);
+		server.broadcast(message);
 	}
 });
-
-var server = ws.createServer({}, httpServer);
 
 server.listen(8000);
